a71c9a36f5937cc08d9b1a647a4bb7b1735b7cea
Eric Hartford <ehartford@gmail.com>
2025-09-08 18:40:13 -0400

    fix build

--- examples/debug_fetch_local.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "helpers/helpers.h"
-int main(){
-  const char* gzpath = "build/testdata.txt.gz";
-  char out[512]={0};
-  int rc = tg_fetch(gzpath, NULL, NULL, 1, 0, out, sizeof(out));
-  printf("tg_fetch rc=%d out='%s'\n", rc, out);
-  return 0;
-}
--- examples/debug_gunzip.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "compat/fetch.h"
-int main(){
-  const char* in = "build/testdata.txt.gz";
-  const char* out = "build/testdata.txt.gz.gunzip_dbg";
-  int rc = tg_gunzip_impl(in, out);
-  printf("tg_gunzip_impl rc=%d\n", rc);
-  return rc!=0;
-}
